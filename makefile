d4972c4cb8d196acce3468eb4fd438e391033fbb
alexweininger <alex.weininger@live.com>
2018-09-23 14:31:52 -0700

    uncover function and process_click function working properly

--- makefile
+++ makefile
@@ -1,17 +1,14 @@
 game: main.o minesweep.o load.o
-	gcc -o game main.o minesweep.o load.o
+	gcc -g -o game main.o minesweep.o load.o
 
 main.o: main.c
-	gcc -c main.c
+	gcc -g -c main.c
 
-minesweep.o:
-	gcc -c minesweep.c
+minesweep.o: minesweep.c minesweep.h
+	gcc -g -c minesweep.c
 
-load.o:
-	gcc -c load.c
-
-minesweep.c: minesweep.h
-load.c: load.h
+load.o: load.c load.h
+	gcc -g -c load.c
 
 
 clean:
--- minesweep.c
+++ minesweep.c
@@ -26,41 +26,48 @@ int play_game(char *in) {
     int c = process_click(board, row, col, move);
 
     if (c == 0) {
-      printf("Game over! You lose!\n");
       gameOver = 1;
     }
   }
+
+  free_game(board);
 }
 
-int process_click(game *board, int x, int y, int move) {
-  x--;
-  y--;
+int process_click(game *board, int row, int col, int move) {
 
-  cell *c = &board->cells[x][y];
+  row--;
+  col--;
+
+  if (!is_safe(row, col, board->row_max, board->col_max)) {
+    printf("Invalid input.\n");
+    return 0;
+  }
+
+  cell *c = &board->cells[row][col];
 
   if (move == 1) {
     c->color = black;
-  } else if (c->color == gray && c->mine < 0) {
-    c->color = white;
-  } else if (c->mine == 1) {
-    printf("Game Over\n");
+  } else if (c->mine < 0) {
     return 0;
-  } else {
+  } else if (c->color == gray) {
 
-    int adjx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-    int adjy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    c->color = white;
 
-    int x1, y1;
+    if (c->mine == 0) {
 
-    int i = 0;
-    for (i = 0; i < 8; i++) {
+      int rowDelta[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+      int colDelta[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 
-      for (int j = 0; j < 8; j++) {
-        x1 = x + adjx[j];
-        y1 = y + adjy[j];
+      int newRow, newCol;
 
-        if (is_safe(x1, y1, board->row_max, board->col_max)) {
-          return uncover(board, x1, y1);
+      int i = 0;
+      for (i = 0; i < 8; i++) {
+
+        newRow = row + rowDelta[i];
+        newCol = col + colDelta[i];
+
+        if (is_safe(newRow, newCol, board->row_max, board->col_max)) {
+          uncover(board, newRow, newCol);
         }
       }
     }
@@ -68,19 +75,49 @@ int process_click(game *board, int x, int y, int move) {
 }
 
 int is_safe(int row, int col, int row_max, int col_max) {
-  if (row < 0 || row > row_max || col < 0 || col > col_max) {
+  if (row < 0 || row >= row_max || col < 0 || col >= col_max) {
     return 0;
   } else {
     return 1;
   }
 }
-
+/*
+ * TODO done
+ */
 int uncover(game *board, int row, int col) {
+  print_game(board);
 
-  if (board->cells[row][col].color == black) {
+  if (!is_safe(row, col, board->row_max, board->col_max)) {
+    return 1;
+  } else if (board->cells[row][col].mine > 0) {
+    board->cells[row][col].color = white;
+    return 1;
+  }
+
+  if (board->cells[row][col].color == white) {
+    return 1;
+  } else if (board->cells[row][col].color == black) {
     return 0;
-  } else if (board->cells[row][col].color == gray) {
-    // TODO
+  } else {
+    // color is gray
+    board->cells[row][col].color = white;
+
+    int rowDelta[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    int colDelta[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+    int newRow, newCol;
+
+    int i, unc;
+    for (i = 0; i < 8; i++) {
+
+      newRow = row + rowDelta[i];
+      newCol = col + colDelta[i];
+
+      if (is_safe(newRow, newCol, board->row_max, board->col_max)) {
+        uncover(board, newRow, newCol);
+      }
+    }
+    return unc;
   }
 }
 
@@ -108,18 +145,26 @@ void print_game(game *board) {
   int i, j;
   for (i = 0; i < board->row_max; i++) {
     for (j = 0; j < board->col_max; j++) {
-      if (board->cells[i][j].color == white && board->cells[i][j].mine < 2) {
+      if (board->cells[i][j].color == white && board->cells[i][j].mine == 0) {
         printf("0 ");
       } else if (board->cells[i][j].color == black) {
         printf("B ");
       } else if (board->cells[i][j].color == gray) {
         printf("E ");
       } else if (board->cells[i][j].color == white) {
-        printf("%1d", board->cells[i][j].mine);
+        printf("%-d ", board->cells[i][j].mine);
       }
     }
 
     printf("\n");
   }
   printf("\n");
+}
+
+void free_game(game *board) {
+  int i = 0;
+  for (i = 0; i < board->row_max; i++) {
+    free(board->cells[i]);
+  }
+  free(board);
 }
